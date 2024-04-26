1f9492bed1be2983a15b418e92ba513a073712bb
Francilândio <francilandiolima@alu.ufc.br>
2024-04-26 10:17:41 -0300

    Entrega final

--- src/index.js
+++ src/index.js
@@ -1,1 +1,39 @@
-vvygvgyyvygv
+function calculaNivel(vitorias, derrotas){
+    let saldoVitorias = vitorias - derrotas;
+    let nivel = ""
+
+    switch(true){
+        case vitorias <= 10:
+            nivel = "Ferro";
+            break;
+        case vitorias <= 20:
+            nivel = "Bronze";
+            break;
+        case vitorias <= 50:
+            nivel = "Prata";
+            break;
+        case vitorias <= 80:
+            nivel = "Ouro";
+            break;
+        case vitorias <= 90:
+            nivel = "Diamante";
+            break;
+        case vitorias <= 100:
+            nivel = "Lendário";
+            break;
+        case vitorias >= 101:
+            nivel = "Imortal";
+            break;
+        default:
+            nivel = "";
+            break;
+
+    }
+
+    console.log("O Herói tem de saldo de "+saldoVitorias+" está no nível de "+nivel);
+}
+
+let vitorias = 102;
+let derrotas = 4;
+
+calculaNivel(vitorias, derrotas);
